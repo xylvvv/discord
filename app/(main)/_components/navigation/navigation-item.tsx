0a438de3fbe2bbe053fc0d7456953953a4f210a7
lxy <lxy2011w@163.com>
2023-10-07 02:02:14 +0800

    Navigation Sidebar

---
+++ app/(main)/_components/navigation/navigation-item.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Image from "next/image";
+import { useParams, useRouter } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+import ActionTooltip from "@/components/action-tooltip";
+
+interface NavigationItemProps {
+  id: string;
+  imageUrl: string;
+  name: string;
+}
+
+const NavigationItem = ({
+  id,
+  imageUrl,
+  name,
+}: NavigationItemProps) => {
+  const params = useParams();
+  const router = useRouter()
+
+  return (
+    <ActionTooltip
+      side="right"
+      align="center"
+      label={name}
+    >
+      <button
+        className="group relative flex items-center"
+        onClick={() => router.push(`/servers/${id}`)}
+      >
+        <div
+          className={cn(
+            "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
+            params?.serverId !== id ? "group-hover:h-[20px] h-[8px]" : "h-[36px]"
+          )}
+        />
+        <div
+          className={cn(
+            "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
+            params?.serverId === id && "bg-primary/10 text-primary rounded-[16px]"
+          )}
+        >
+          <Image
+            fill
+            src={imageUrl}
+            alt="Server"
+          />
+        </div>
+      </button>
+    </ActionTooltip>
+  );
+}
+ 
+export default NavigationItem;
--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -12,7 +12,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
-export function ModeToggle() {
+const ModeToggle = () => {
   const { setTheme } = useTheme()
 
   return (
@@ -37,4 +37,6 @@ export function ModeToggle() {
       </DropdownMenuContent>
     </DropdownMenu>
   )
-}
+};
+
+export default ModeToggle;
